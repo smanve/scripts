94a02265b7f44e5b0f46e5fc8eb282450769cd7b
smanve <msin0017@student.monash.edu>
2023-12-01 17:59:55 +1100

    completed the bash code for configuing GCM

--- configuration.sh
+++ configuration.sh
@@ -8,8 +8,16 @@ echo "Configuring git"
 echo "Please enter your git username: "
 read USERNAME
 PRIVATE_EMAIL="$USERNAME@users.noreply.github.com"
-read -s -p "Please enter your git email [Press enter to accept the private email $PRIVATE_EMAIL]: " EMAIL
+read -p "Please enter your git email [Press enter to accept the private email $PRIVATE_EMAIL]: " EMAIL
 
 # Using parameter expansion to provide default values
 EMAIL="${EMAIL:-${PRIVATE_EMAIL}}"
 
+echo "Configuring git with username: $USERNAME and email: $EMAIL"
+git config --global user.name "$USERNAME"
+git config --global user.email "$EMAIL"
+
+echo "Configuring git to use the credential manager"
+git config --global credential.helper 'cache --timeout=3600'
+
+echo "Git configuration complete."
